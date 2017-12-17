e29a77ebe6fdcd699312c0eafeac636958af9602
Simon Clériot <cleriotsimon@gmail.com>
2017-12-17 20:03:44 +0100

    Change Seance to TD + TD 6

---
+++ td/td6/src/etape3_express.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+
+const app = express()
+app.use(bodyParser.json()) //parse JSON body
+app.use(bodyParser.urlencoded()) //parse x-www-form-urlencoded body
+
+app.use((req, res, next) => {
+    console.log(new Date(), req.path, req.body);
+    req.requestTime = new Date();
+    next();
+});
+
+app.get('/foo/:myvar', (req, res) => res.send({var: req.params.myvar}))
+
+app.post('/add', (req, res) => {
+    res.send(`${parseInt(req.body.foo)+parseInt(req.body.bar)}`)
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
